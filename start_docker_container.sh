9a6fd3e32e870521bc044dd9ea2cd9383ac8c8e8
Luthov <lukettl15@gmail.com>
2023-12-12 15:10:03 +0800

    init commit

---
+++ start_docker_container.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+container_name=ASME_ROS
+
+if [ "$(docker ps -aq -f status=exited -f name=${container_name})" ]
+then
+	docker start ${container_name}
+fi
+
+docker exec -it ${container_name} /bin/zsh
